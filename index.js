df886297ab8b78d96ecf65e3e5dfa4aea37ee31d
Riku Koskinen <riku.i.koskinen@gmail.com>
2025-02-05 17:04:55 +0200

    Tehtävä 3.7 puhelinluettelon backend, step7, VALMIS

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
-app.use(express.json())
+app.use(express.json()) //json-parseri
+app.use(morgan('tiny')) //morgan loggaa konsoliin pyyntöjen tiedot
 
 const PORT = 3001
 app.listen(PORT)
