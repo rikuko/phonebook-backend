b1b7fe81604f40430e8bea80efcf5e170a9c1486
Riku Koskinen <riku.i.koskinen@gmail.com>
2025-02-09 13:24:18 +0200

    ***

--- index.js
+++ index.js
@@ -110,6 +110,8 @@ app.post('/api/persons', postLogger, (request, response) => {
 })
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
 
