46808c854a0bf5712f7dec18f2e7fc228f85ca08
Riku Koskinen <riku.i.koskinen@gmail.com>
2025-03-13 17:06:53 +0200

    Tehtävä 3.17*, puehlinluettelo ja tietokanta, step5, VALMIS

--- index.js
+++ index.js
@@ -4,29 +4,6 @@ const Contact = require('./models/contact')
 
 const app = express()
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": "1"
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": "2"
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": "3"
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": "4"
-    }
-]
-
 const morgan = require('morgan')
 const cors = require('cors')
 
